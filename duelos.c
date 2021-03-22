84608036c4e6d9c0108a421475d4dc5bbeac63e2
Lorena Leão <42307217+lorenaleao@users.noreply.github.com>
2021-03-22 03:47:41 -0300

    Add TP3 description

--- duelos.c
+++ duelos.c
@@ -1,3 +1,71 @@
+/*
+Duelo
+Disponível a partir de: quinta, 15 Set 2016, 15:00
+Número máximo de arquivos: 1
+Tamanho máximo de arquivo carregado: 512 KiB
+Tipo de trabalho: Trabalho individual
+
+Duelo
+
+DATA DE ENTREGA: 25/09/2016 (Entregas após esta data serão penalizadas)
+Nome do arquivo fonte na linguagem C ANSI.
+Você deve anexar o arquivo código-fonte ao invés de um arquivo compactado. Exemplo: duelo.c
+
+Descrição
+
+A ideia dessa tarefa é implementar um jogo de duelos como os que ocorriam no velho oeste americano. Para quem nunca ouviu falar, num duelo duas pessoas param de frente uma para a outra e atiram, quem acertar o outro ganha. Porém, nesse jogo os duelistas irão atirar com palavras. Ao atirar as palavras, todas as letras que são iguais se anulam e apenas as letras que não são comuns entre as palavras acertam o adversário. 
+
+Tarefa
+
+Seu desafio é criar um programa que dadas duas palavras, você deve eliminar as letras em comuns na taxa de 1 para 1 e produzir o conjunto de letras que sobram após a colisão entre as duas palavras. Com o conjunto de letras restantes, some o valor do código ASCII de cada letra restante para obter a pontuação final. Ao fim a palavra com o maior número de pontos é a vencedora.
+
+Entrada
+
+Um inteiro N referindo ao número de duelos que ocorrerão. As demais linhas são compostas por duas palavras separadas por apenas um espaço em branco definindo o atirador do lado esquerdo e o atirador do lado direito.
+
+Saída
+
+A pontuação de cada uma das palavras seguidas pela indicação do vencedor. O vencedor é definido pela seguinte frase: “palavra” (esquerda || direita) ganha. Em caso de empate deve ser impresso : houve um empate.
+
+Exemplo
+
+Entrada
+
+4
+cama camarao
+gato rato
+critico otimo
+dunia bupia
+
+Saída
+
+Duelo 1
+pontos esquerda: 0
+pontos direita: 322
+"camarao" (direita) ganha
+
+Duelo 2
+pontos esquerda: 103
+pontos direita: 114
+"rato" (direita) ganha
+
+Duelo 3
+pontos esquerda: 417
+pontos direita: 220
+"critico" (esquerda) ganha
+
+Duelo 4
+pontos esquerda: 210
+pontos direita: 210
+houve um empate
+
+Restrições
+
+    Será aceito apenas duas palavras separadas por um caractere de espaço.
+    Cada caractere de uma palavra será comparado a apenas um caractere na outra palavra. Ou seja, ao comparar uma letra “a” com uma palavra contendo duas letras “a” o saldo final é de uma letra “a”. “a” - “aa” = “a”
+    As palavras não contêm acentos ou letras maiúsculas. 
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
