c438d0ba59c0b1173f5583f21d0d269aff829058
Lorena Leão <42307217+lorenaleao@users.noreply.github.com>
2021-03-22 03:35:39 -0300

    Add TP1 description

--- TP1_QuadroMágico.c
+++ TP1_QuadroMágico.c
@@ -1,3 +1,56 @@
+/*
+Quadro Mágico
+Disponível a partir de: terça, 6 Set 2016, 06:00
+Número máximo de arquivos: 1
+Tamanho máximo de arquivo carregado: 512 KiB
+Tipo de trabalho: Trabalho individual
+Quadro Mágico
+
+DATA DE ENTREGA: 16/09/2016 (Entregas após esta data serão penalizadas)
+Nome do arquivo fonte na linguagem C ANSI.
+Você deve anexar o arquivo código-fonte ao invés de um arquivo compactado. Exemplo: quadro.c
+
+Senhor Mário é conhecido mundialmente pela fabricação de quadros mágicos de dimensões 3X3. Um quadro é chamado mágico quando a soma dos elementos de uma determinada linha, coluna ou diagonal é sempre igual. Infelizmente, assaltantes invadiram recentemente a oficina do Sr. Mário e roubaram alguns dos números de seus quadros mágicos. Felizmente os meliantes não conseguiram roubar mais do que um número de cada quadro. Desesperado, pois devia entregar os quadros naquele dia, o Sr. Mário veio procurar a sua ajuda para tentar completar os quadros com os números faltantes.
+Tarefa
+
+Escreva um programa que, dado um quadro com algum número faltando, marcado com o algarismo zero, determine qual era o quadro mágico original. 
+Entrada
+
+A entrada contém um único conjunto de testes, que deve ser lido do dispositivo de entrada padrão (normalmente o teclado). A entrada contém três linhas, cada uma contendo três inteiros N(0 <= N <= 1000). O número zero representa o número roubado.
+Saída
+
+Seu programa deve imprimir, na saída padrão, três linhas, cada uma contendo três inteiros, descrevendo a configuração original do quadro mágico.
+
+Exemplo 1
+
+Entrada
+
+4 9 2
+3 0 7
+8 1 6
+
+Saída
+
+4 9 2
+3 5 7
+8 1 6
+
+Exemplo 2
+
+Entrada
+
+6 12 12
+16 10 0
+8 8 14
+
+Saída
+
+6 12 12
+16 10 4
+8 8 14
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
